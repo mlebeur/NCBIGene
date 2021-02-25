9df193366255bb47bbd70045e6306c7b497c2a51
LEBEURRIER Manuel <lebeurrier.manuel@hotmail.fr>
2021-02-25 15:58:56 +0100

    Update parser.py

--- parser.py
+++ parser.py
@@ -4,13 +4,9 @@ from biothings.utils.dataload import dict_convert, dict_sweep
 from biothings import config
 logging = config.logger
 def load_NCBIGene(data_folder):
-    infileInfo = os.path.abspath("/opt/biothings/GRCh37/ncbi_gene/latest/NcbiGeneInfo.tsv")
-    infileID = os.path.abspath("/opt/biothings/GRCh37/ncbi_gene/latest/NcbiGeneRif.tsv")
-    assert os.path.exists(infileInfo)
-    assert os.path.exists(infileID)
-    datInfo = pandas.read_csv(infileInfo,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE)
-    datID = pandas.read_csv(infileID,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE)
-    dat = datID.join(datInfo.set_index('entrez_id'), on='entrez_id').to_dict(orient='records')
+    infile = os.path.abspath("/opt/biothings/GRCh37/ncbi_gene/latest/NcbiGeneInfo.tsv")
+    assert os.path.exists(infile)
+    dat = pandas.read_csv(infile,sep="\t",squeeze=True,quoting=csv.QUOTE_NONE).to_dict(orient='records')
     results = {}
     for rec in dat:
         _id = rec["entrez_id"]
